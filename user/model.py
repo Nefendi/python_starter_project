0b4113fc248ecdf38ada5ef34ba30835b9716269
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-07-01 16:16:26 +0200

    Add User DB model

---
+++ user/model.py
@@ -0,0 +1,14 @@
+from uuid import UUID
+
+from sqlalchemy.dialects.postgresql import UUID as PSQL_UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database.db import Base
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(PSQL_UUID(as_uuid=True), primary_key=True)
+    name: Mapped[str]
+    surname: Mapped[str]
