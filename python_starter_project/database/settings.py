6f060c64d0325279c8424fb16a5f571f46e6f3cf
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-07-02 23:24:44 +0200

    Integrate mypy with pydantic more tightly
    A plugin which pydantic ships with has been toggled on.

--- python_starter_project/database/settings.py
+++ python_starter_project/database/settings.py
@@ -1,13 +1,20 @@
-from pydantic import BaseSettings, Field, PostgresDsn
+from typing import TYPE_CHECKING
+
+from pydantic import BaseSettings
+
+# An ugly workaround for this: https://github.com/pydantic/pydantic/issues/1490
+if TYPE_CHECKING:
+    PostgresDsn = str
+else:
+    from pydantic import PostgresDsn
 
 
 class DbSettings(BaseSettings):
-    URL: PostgresDsn = Field(
-        default=(
-            "postgresql://python_starter_project:password"
-            "@127.0.0.1:5432/python_starter_project"
-        )
+    URL: PostgresDsn = (
+        "postgresql://python_starter_project:password"
+        "@127.0.0.1:5432/python_starter_project"
     )
 
     class Config:
         env_prefix = "DB_CONFIG_"
+        case_sensitive = True
