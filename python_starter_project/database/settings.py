f11fe12496e14b896ee8bbec9cc0a2db64dbd878
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-07-02 11:14:57 +0200

    Fix PostgresDsn field in DbSettings

--- python_starter_project/database/settings.py
+++ python_starter_project/database/settings.py
@@ -1,10 +1,12 @@
-from pydantic import BaseSettings, PostgresDsn
+from pydantic import BaseSettings, Field, PostgresDsn
 
 
 class DbSettings(BaseSettings):
-    URL: PostgresDsn = PostgresDsn(
-        "postgresql://python-starter-project:password"
-        "@127.0.0.1:5432/python-starter-project"
+    URL: PostgresDsn = Field(
+        default=(
+            "postgresql://python-starter-project:password"
+            "@127.0.0.1:5432/python-starter-project"
+        )
     )
 
     class Config:
